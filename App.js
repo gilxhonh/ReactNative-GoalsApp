451050c713edea1773ce0d072f9eda1f2dd44a0f
Gilxhon <gilxhonhima@gmail.com>
2023-03-15 10:17:08 +0100

    add feature to display inputted goals on the screen

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput, ScrollView} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [input, setInput] = useState("");
+    const [goals, setGoals] = useState([]);
+
+    let handeInputGoal = (enteredText) => {
+        setInput(enteredText);
+    }
+
+    let addGoal = () => {
+        console.log(input);
+        setGoals(currentCourseGoals => [
+            ...currentCourseGoals,
+            input
+        ]);
+        setInput("");
+    }
+
+
+
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    onChangeText={handeInputGoal}
+                    style={styles.textInput}
+                    placeholder='Your course goal!'
+                    value={input}
+                />
+                <Button onPress={addGoal} title='Add Goal'/>
+            </View>
+
+            <View style={styles.goalsContainer}>
+                <ScrollView alwaysBounceVertical={false}>
+                    {goals.map((goal) => (
+                        <View style={styles.goalItem} key={goal}>
+                            <Text style={styles.goalText}>{goal}</Text>
+                        </View>
+                    ))}
+                </ScrollView>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+        padding: 50,
+        paddingHorizontal: 16,
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#02987e',
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#02987e',
+        width: '80%',
+        marginRight: 16,
+        padding: 8,
+    },
+    goalsContainer: {
+        flex: 4,
+    },
+    goalItem: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: '#02987e',
+    },
+    goalText: {
+        color: '#fff',
+    }
 });
